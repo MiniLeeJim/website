c410f186742917fcf44d373eee190eaab8e4dfb3
MiniLeeJim <105410443+MiniLeeJim@users.noreply.github.com>
2022-05-25 15:32:24 +0900

    슬라이드 추가

--- project/js/youngjin.js
+++ project/js/youngjin.js
@@ -1,3 +1,64 @@
+//sec1 배너
+var bannerSwiper = new Swiper(".sec1 .inner", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    speed : 800,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    }
+});
+
+//sec3 신규도서
+var newbookSwiper = new Swiper(".sec3 .book-slide", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+//이기적 베스트
+var bestbookSwiper = new Swiper(".sec4 .book-slide", {
+    loop: true,
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    }
+});
+
+//공지, 정오 탭
 $(".sec5 .tap > h2").click(function(){
     $(this).addClass('on').siblings().removeClass('on');
     $("#"+$(this).data('id')).addClass('on').siblings().removeClass('on');
